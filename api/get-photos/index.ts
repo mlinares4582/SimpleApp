e2312d0540d3b30afa39e6afd7b86bf31f3491ed
MichaelLinares <mglinares.2419@gmail.com>
2022-06-13 10:34:20 -0400

    API Section

--- api/get-photos/index.ts
+++ api/get-photos/index.ts
@@ -2,14 +2,38 @@ import { APIGatewayProxyEventV2,
          APIGatewayProxyResultV2,
          Context } 
 from "aws-lambda";
+import {S3, SES} from 'aws-sdk';
+import { Filename } from "aws-sdk/clients/elastictranscoder";
 
-const bucketName = process.env.PHOTO_BUCKET_NAME;
+const s3 = new S3();
+const bucketName = process.env.PHOTO_BUCKET_NAME!;
+
+async function generateUrl(object: S3.Object): Promise<{filename: string, url: string}>{
+  const url = await s3.getSignedUrlPromise('getObject', {
+    Bucket: bucketName,
+    Key: object.Key!,
+    Expires: 24 * 60 * 60,
+  });
+  return{
+    filename: object.Key!,
+    url,
+  };
+}
 
 async function getPhotos(event: APIGatewayProxyEventV2, context: Context):Promise<APIGatewayProxyResultV2> {
-  console.log("I got the bucket Bane and it is " + bucketName);
-  return{
-    statusCode: 200,
-    body:'Hello from lambda, it is live!'
+  console.log("I got the bucket name and it is " + bucketName);
+  try {
+    const {Contents: results} = await s3.listObjects( {Bucket: bucketName} ).promise();
+    const photos = await Promise.all(results!.map((results) => generateUrl(results)))
+    return{
+      statusCode: 200,
+      body:JSON.stringify(photos),
+    };
+  } catch (error) {
+    return{
+      statusCode: 500,
+      body: JSON.stringify(error),
+    }
   }
 
 }
--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -2,9 +2,14 @@ import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { HttpMethod, Runtime } from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { CorsHttpMethod, HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+
+
 
 
 export class SimpleAppStack extends Stack {
@@ -31,6 +36,31 @@ export class SimpleAppStack extends Stack {
         PHOTO_BUCKET_NAME: bucket.bucketName,
       },
     });
+
+    const bucketContainerPermissions = new PolicyStatement();
+    bucketContainerPermissions.addResources(bucket.bucketArn);
+    bucketContainerPermissions.addActions('s3:ListBucket');
+
+
+    const bucketPermissions = new PolicyStatement();
+    bucketPermissions.addResources(`${bucket.bucketArn}/*`);
+    bucketPermissions.addActions('s3:GetObject', 's3:PutObject');
+
+    getPhotos.addToRolePolicy(bucketContainerPermissions);
+    getPhotos.addToRolePolicy(bucketPermissions);
+
+    const httpApi = new HttpApi(this, 'MySimpleAppHttpApi', {
+      corsPreflight: {
+        allowOrigins: ['*'],
+        allowMethods:[CorsHttpMethod.GET],
+      },
+      apiName:'photo-api',
+      createDefaultStage: true
+    });
+
+    // const lambdaIntegration = new LambdaIntegration({
+    //   handler: getPhotos
+    // });
 
     new CfnOutput(this, 'MySimpleAppBucketNameExport', {
       value: bucket.bucketName,
